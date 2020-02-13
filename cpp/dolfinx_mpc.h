df83cdd0c966fd4f1caed47089544e433bc6e428
dokken <dokken92@gmail.com>
2020-02-13 21:49:42 +0000

    Started flattening

---
+++ cpp/dolfinx_mpc.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// DOLFINX_MPC interface
+
+#include <dolfinx_mpc.h>
+#include <MultiPointConstraint.h>
--- cpp/dolfinx_mpc/dolfinx_mpc.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// DOLFINX interface
-
-#include <dolfinx_mpc/dolfinx_mpc.h>
-#include <dolfinx_mpc/MultiPointConstraint.h>
