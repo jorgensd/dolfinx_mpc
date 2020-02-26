095ce4b922ff8a43495039d9e51b2a6e8645647c
Jørgen Dokken <jsd55@cam.ac.uk>
2020-02-26 13:29:21 +0000

    Loads of additions to code to make it work for sub spaces (using block size). At least in serial. More work has to be done for parallel

--- python/dolfinx_mpc/assemble_vector.py
+++ python/dolfinx_mpc/assemble_vector.py
@@ -47,12 +47,14 @@ def assemble_vector(form, multipointconstraint,
 
     # Get index map and ghost info
     index_map = multipointconstraint.index_map()
-    ghost_info = (index_map.local_range,
-                  index_map.indices(True), index_map.ghosts)
+
+    ghost_info = (index_map.local_range, index_map.indices(True),
+                  index_map.block_size, index_map.ghosts)
 
     vector = dolfinx.cpp.la.create_vector(index_map)
 
     ufc_form = dolfinx.jit.ffcx_jit(form)
+
     kernel = ufc_form.create_cell_integral(-1).tabulate_tensor
     gdim = V.mesh.geometry.dim
     num_dofs_per_element = V.dofmap.dof_layout.num_dofs
@@ -77,12 +79,11 @@ def assemble_vector_numba(b, kernel, mesh, x, gdim, facet_index,
     (edge_reflections, face_reflections,
      face_rotations, facet_permutations) = permutation_data
 
-    local_range, global_indices, ghosts = ghost_info
-
+    local_range, global_indices, block_size, ghosts = ghost_info
+    local_size = local_range[1] - local_range[0]
     connections, pos = mesh
     geometry = numpy.zeros((pos[1]-pos[0], gdim))
     coeffs = numpy.zeros(1, dtype=PETSc.ScalarType)
-    constants = numpy.zeros(1, dtype=PETSc.ScalarType)
     constants = numpy.ones(1, dtype=PETSc.ScalarType)
     b_local = numpy.zeros(num_dofs_per_element, dtype=PETSc.ScalarType)
     index = 0
@@ -141,18 +142,25 @@ def assemble_vector_numba(b, kernel, mesh, x, gdim, facet_index,
                             c0 = cell_coeffs[m_0]
                             # Map to local index
                             local_index = -1
-                            in_range = (cell_masters[m_0] < local_range[1] and
-                                        cell_masters[m_0] >= local_range[0])
+                            in_range = (cell_masters[m_0] <
+                                        block_size * local_range[1] and
+                                        cell_masters[m_0] >=
+                                        block_size * local_range[0])
                             if in_range:
-                                local_index = cell_masters[m_0]-local_range[0]
+                                local_index = (cell_masters[m_0] -
+                                               block_size * local_range[0])
                             else:
                                 # Inverse mapping from ghost info
                                 for q, ghost in enumerate(ghosts):
-                                    if cell_masters[m_0] == ghost:
-                                        local_index = q + \
-                                            local_range[1]-local_range[0]
-                                        # break
-                                        pass
+                                    if local_index != -1:
+                                        break
+                                    for comp in range(block_size):
+                                        cm0 = cell_masters[m_0]
+                                        if cm0 == block_size * ghost + comp:
+                                            local_index = ((q + local_size)
+                                                           * block_size + comp)
+                                            break
+
                             assert local_index != -1
                             b[local_index] += c0*b_local_copy[k]
                             b_local[k] = 0
@@ -164,3 +172,46 @@ def assemble_vector_numba(b, kernel, mesh, x, gdim, facet_index,
     #     if bcs[k]:
 
     #         b[k] = values[k]
+
+
+@numba.njit
+def block_numba(b, kernel, mesh, x, gdim, facet_index,
+                permutation_data, dofmap, num_dofs_per_element,
+                mpc, ghost_info, bcs):
+    """Assemble provided FFC/UFC kernel over a mesh into the array b"""
+    ffi_fb = ffi.from_buffer
+    (bcs, values) = bcs
+    (slaves, masters, coefficients, offsets,
+     slave_cells, cell_to_slave, cell_to_slave_offset) = mpc
+    (edge_reflections, face_reflections,
+     face_rotations, facet_permutations) = permutation_data
+
+    local_range, global_indices, block_size, ghosts = ghost_info
+    connections, pos = mesh
+    geometry = numpy.zeros((pos[1]-pos[0], gdim))
+    coeffs = numpy.zeros(2, dtype=PETSc.ScalarType)
+    constants = numpy.zeros(2, dtype=PETSc.ScalarType)
+    constants = numpy.ones(2, dtype=PETSc.ScalarType)
+    b_local = numpy.zeros(num_dofs_per_element, dtype=PETSc.ScalarType)
+
+    for i, cell in enumerate(pos[:-1]):
+        num_vertices = pos[i + 1] - pos[i]
+        c = connections[cell:cell + num_vertices]
+        for j in range(num_vertices):
+            for k in range(gdim):
+                geometry[j, k] = x[c[j], k]
+        b_local.fill(0.0)
+        # Cell orientation data
+        face_reflection = face_reflections[i, :]
+        # FIXME: Numba does not support edge reflections
+        edge_reflection = edge_reflections  # edge_reflections[i,:]
+        face_rotation = face_rotations[i, :]
+
+        kernel(ffi_fb(b_local), ffi_fb(coeffs),
+               ffi_fb(constants),
+               ffi_fb(geometry), ffi_fb(facet_index),
+               ffi_fb(facet_permutations), ffi_fb(face_reflection),
+               ffi_fb(edge_reflection), ffi_fb(face_rotation))
+
+        for j in range(num_dofs_per_element):
+            b[dofmap[i * num_dofs_per_element + j]] += b_local[j]
--- python/dolfinx_mpc/multipointconstraint.py
+++ python/dolfinx_mpc/multipointconstraint.py
@@ -1,5 +1,4 @@
 import typing
-
 import numba
 from petsc4py import PETSc
 import types
@@ -16,30 +15,34 @@ def backsubstitution(mpc, vector, dofmap):
     slave_cells = mpc.slave_cells()
     cell_to_slave, cell_to_slave_offset = mpc.cell_to_slave_mapping()
     ghost_info = (index_map.local_range, index_map.ghosts,
-                  index_map.indices(True))
+                  index_map.block_size, index_map.indices(True))
     mpc = (slaves, slave_cells, cell_to_slave, cell_to_slave_offset,
            masters, coefficients, offsets)
-
-    backsubstitution_numba(vector, dofmap.dof_array, mpc, ghost_info)
+    num_dofs_per_element = dofmap.dof_layout.num_dofs
+    backsubstitution_numba(vector, dofmap.dof_array,
+                           num_dofs_per_element, mpc, ghost_info)
     vector.ghostUpdate(addv=PETSc.InsertMode.INSERT,
                        mode=PETSc.ScatterMode.FORWARD)
     return vector
 
 
 @numba.njit
-def backsubstitution_numba(b, dofmap, mpc, ghost_info):
+def backsubstitution_numba(b, dofmap, num_dofs_per_element, mpc, ghost_info):
     """
         Insert mpc values into vector bc
         """
     (slaves, slave_cells, cell_to_slave, cell_to_slave_offset,
      masters, coefficients, offsets) = mpc
-    (local_range, ghosts, global_indices) = ghost_info
+    (local_range, ghosts, block_size, global_indices) = ghost_info
+    local_size = local_range[1] - local_range[0]
     slaves_visited = numpy.empty(0, dtype=numpy.float64)
     # Loop through slave cells
     for (index, cell_index) in enumerate(slave_cells):
         cell_slaves = cell_to_slave[cell_to_slave_offset[index]:
                                     cell_to_slave_offset[index+1]]
-        local_dofs = dofmap[3 * cell_index:3 * cell_index + 3]
+        local_dofs = dofmap[num_dofs_per_element * cell_index:
+                            num_dofs_per_element * cell_index
+                            + num_dofs_per_element]
 
         # Find the global index of the slaves on the cell in the slaves-array
         global_slaves_index = []
@@ -65,22 +68,29 @@ def backsubstitution_numba(b, dofmap, mpc, ghost_info):
                 for (master, coeff) in zip(slaves_masters, slaves_coeffs):
                     # Find local index for master
                     local_master_index = -1
-                    if master < local_range[1] and master >= local_range[0]:
-                        local_master_index = master-local_range[0]
+                    in_range = (master < block_size * local_range[1] and
+                                master >= block_size * local_range[0])
+                    if in_range:
+                        local_master_index = master-block_size*local_range[0]
                     else:
                         for q, ghost in enumerate(ghosts):
-                            if master == ghost:
-                                local_master_index = q + \
-                                    local_range[1] - local_range[0]
+                            if local_master_index != -1:
                                 break
-                    assert q != -1
+                            for comp in range(block_size):
+                                if master == block_size * ghost + comp:
+                                    local_master_index = ((q + local_size)
+                                                          * block_size + comp)
+                                    break
+                    assert local_master_index != -1
                     b[k] += coeff*b[local_master_index]
 
 
 def slave_master_structure(V: function.FunctionSpace, slave_master_dict:
                            typing.Dict[types.FunctionType,
                                        typing.Dict[
-                                           types.FunctionType, float]]):
+                                           types.FunctionType, float]],
+                           subspace_slave=None,
+                           subspace_master=None):
     """
     Returns the data structures required to build a multi-point constraint.
     Given a nested dictionary, where the first keys are functions for
@@ -100,16 +110,38 @@ def slave_master_structure(V: function.FunctionSpace, slave_master_dict:
     masters = []
     coeffs = []
     offsets = []
-    local_min = V.dofmap.index_map.local_range[0]
+    local_min = (V.dofmap.index_map.local_range[0]
+                 * V.dofmap.index_map.block_size)
+    if subspace_slave is not None:
+        Vsub_slave = V.sub(subspace_slave).collapse()
+    if subspace_master is not None:
+        Vsub_master = V.sub(subspace_master).collapse()
     for slave in slave_master_dict.keys():
         offsets.append(len(masters))
+        if subspace_slave is None:
+            dof = fem.locate_dofs_geometrical(V, slave) + local_min
+            dof_global = numpy.vstack(MPI.comm_world.allgather(dof))[0]
 
-        dof = fem.locate_dofs_geometrical(V, slave) + local_min
-        dof_global = numpy.vstack(MPI.comm_world.allgather(dof))[0]
+        else:
+            dof = fem.locate_dofs_geometrical((V.sub(subspace_slave),
+                                               Vsub_slave),
+                                              slave)
+            for (i, d) in enumerate(dof):
+                dof[i] += local_min
+            dof_global = numpy.vstack(MPI.comm_world.allgather(dof))[0, 0]
         slaves.append(dof_global)
         for master in slave_master_dict[slave].keys():
-            dof_m = fem.locate_dofs_geometrical(V, master) + local_min
-            dof_m = numpy.vstack(MPI.comm_world.allgather(dof_m))[0]
+            if subspace_master is None:
+                dof_m = fem.locate_dofs_geometrical(V, master) + local_min
+                dof_m = numpy.vstack(MPI.comm_world.allgather(dof_m))[0]
+            else:
+                dof_m = fem.locate_dofs_geometrical((V.sub(subspace_master),
+                                                     Vsub_master),
+                                                    master)
+                for (i, d) in enumerate(dof_m):
+                    dof_m[i] += local_min
+                dof_m = numpy.vstack(MPI.comm_world.allgather(dof_m))[0, 0]
+
             masters.append(dof_m)
             coeffs.append(slave_master_dict[slave][master])
     offsets.append(len(masters))
