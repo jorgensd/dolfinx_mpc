371ab2ab549ed1647a7f281df6a031633ccae9c6
root <dokken92@gmail.com>
2020-04-09 13:41:03 +0000

    Rename file and make it work in parallel

---
+++ python/tests/test_collision.py
@@ -0,0 +1,65 @@
+import dolfinx_mpc.cpp
+import dolfinx
+import dolfinx.geometry as geometry
+import numpy as np
+import pytest
+
+
+@pytest.mark.parametrize("coordinate", [[0, 0, 0], [1, 0, 0], [1, 1, 1]])
+def test_collision_tetrahedron(coordinate):
+    """
+    Test alternative collision algorithm which reconstructs the finite element
+    from dof coordinates
+    """
+    mesh = dolfinx.UnitCubeMesh(dolfinx.MPI.comm_world, 1, 1, 1)
+    tdim = mesh.topology.dim
+
+    V = dolfinx.VectorFunctionSpace(mesh, ("Lagrange", 1))
+    x_coords = V.tabulate_dof_coordinates()
+
+    # Create cell-to-facet-connectivity
+    mesh.create_connectivity_all()
+    tree = dolfinx.geometry.BoundingBoxTree(mesh, tdim)
+    # Find index of coordinate in dof coordinates
+    coordinate_index = -1
+    for i, coord in enumerate(x_coords):
+        if np.allclose(coord, coordinate):
+            coordinate_index = i
+            break
+
+    if coordinate_index != -1:
+        possible_cells = geometry.compute_collisions_point(
+            tree, x_coords[coordinate_index])[0]
+        # Compute collisions with boundingboxtree
+        # Compute actual collisions with mesh
+        actual_collisions = dolfinx.geometry.compute_entity_collisions_mesh(
+            tree, mesh, x_coords[coordinate_index].T)
+        col_2 = dolfinx_mpc.cpp.mpc.check_cell_point_collision(
+            possible_cells, V._cpp_object, x_coords[coordinate_index])
+        second_cells = possible_cells[col_2 == 1]
+
+    # Find index of coordinate in geometry
+    node_index = -1
+    for i, coord in enumerate(mesh.geometry.x):
+        if np.allclose(coord, coordinate):
+            node_index = i
+            break
+    if node_index != -1:
+        # Test same strategy with mesh coordinates
+        possible_cells_mesh = geometry.compute_collisions_point(
+            tree, mesh.geometry.x[node_index])[0]
+        actual_collisions_mesh = geometry.compute_entity_collisions_mesh(
+            tree, mesh, mesh.geometry.x[node_index].T)
+    if node_index != -1 and coordinate_index != -1:
+        assert(np.allclose(possible_cells_mesh, possible_cells))
+        if len(actual_collisions[0]) == len(actual_collisions_mesh[0]):
+            print("Found points in mesh in standard way")
+            assert(np.allclose(
+                actual_collisions[0], actual_collisions_mesh[0]))
+        print("Comparing mesh collisons with FE collisions")
+        assert(np.allclose(second_cells, actual_collisions_mesh[0]))
+    elif node_index == coordinate_index:
+        pass
+    else:
+        print(node_index, coordinate_index)
+        raise RuntimeError("Dofs and geometry lives on different processors.")
