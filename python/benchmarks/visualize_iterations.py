fd7cae3582617a5f69767534471abcd9e4a74ad5
Jorgen Dokken <jsd55@cockatoo.bpi.cam.ac.uk>
2020-07-01 15:40:12 +0100

    Fixes to visualization

--- python/benchmarks/visualize_iterations.py
+++ python/benchmarks/visualize_iterations.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+import matplotlib.transforms as mtransforms
 import numpy as np
 import h5py 
 import mpi4py
@@ -13,5 +15,14 @@ fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 plt.plot(dofs, iterations, "ro")
 ax.set_xscale("log")
+ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+plt.xlabel("# DOFS", fontsize=25)
+plt.ylabel("# Iterations",fontsize=25)
+trans_offset = mtransforms.offset_copy(ax.transData, fig=fig,
+                                       x=0.025, y=0.025, units='inches')
+for i in range(len(iterations)):
+    plt.text(dofs[i], iterations[i], slaves[i], transform=trans_offset, fontsize=20)
+plt.title("MPC Elasticity with BoomerAMG", fontsize=25)
+plt.grid()
 plt.savefig("fig.png")
 
