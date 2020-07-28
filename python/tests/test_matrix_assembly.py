201ccb2ef89e83d89aa3ca2a35347cfd773322d8
root <dokken92@gmail.com>
2020-07-28 17:00:55 +0000

    Added verification to other matrix assembly test

--- python/tests/test_matrix_assembly.py
+++ python/tests/test_matrix_assembly.py
@@ -12,7 +12,6 @@ import numpy as np
 import pytest
 import ufl
 from mpi4py import MPI
-from petsc4py import PETSc
 import dolfinx
 import dolfinx.log
 
@@ -47,21 +46,31 @@ def test_mpc_assembly(master_point, degree, celltype):
                                                    masters, coeffs, offsets,
                                                    master_owners)
 
+    def l2b(li):
+        return np.array(li, dtype=np.float64).tobytes()
+    s_m_c_new = {l2b([1, 0]): {l2b([0, 1]): 0.43,
+                               l2b([1, 1]): 0.11},
+                 l2b([0, 0]):
+                 {l2b(master_point): 0.69}}
+    cc = dolfinx_mpc.create_dictionary_constraint(V, s_m_c_new)
+
     # Assemble custom MPC assembler
-    start = time.time()
-    A_mpc = dolfinx_mpc.assemble_matrix(a, mpc)
-    end = time.time()
-    print("Runtime: {0:.2e}".format(end-start))
+    with dolfinx.common.Timer("~Test: Assemble old"):
+        A_mpc = dolfinx_mpc.assemble_matrix(a, mpc)
+
+    with dolfinx.common.Timer("~Test: Assemble new"):
+        A_cc = dolfinx_mpc.assemble_matrix_local(a, cc)
 
     A_mpc_np = dolfinx_mpc.utils.PETScMatrix_to_global_numpy(A_mpc)
-
+    A_cc_np = dolfinx_mpc.utils.PETScMatrix_to_global_numpy(A_cc)
+    assert(np.allclose(A_mpc_np, A_cc_np))
     # Create globally reduced system with numpy
-    K = dolfinx_mpc.utils.create_transformation_matrix(V, mpc)
+    K = dolfinx_mpc.utils.create_transformation_matrix(V, cc)
     A_org = dolfinx.fem.assemble_matrix(a)
     A_org.assemble()
     A_org_np = dolfinx_mpc.utils.PETScMatrix_to_global_numpy(A_org)
     reduced_A = np.matmul(np.matmul(K.T, A_org_np), K)
-    dolfinx_mpc.utils.compare_matrices(reduced_A, A_mpc_np, slaves)
+    dolfinx_mpc.utils.compare_matrices(reduced_A, A_cc_np, cc)
 
 
 # Check if ordering of connected dofs matter
