c9972deced2f52bb9a63ce382322a62996648209
Jørgen Dokken <jsd55@cam.ac.uk>
2020-11-26 14:00:56 +0000

    Nicer plots

--- python/benchmarks/post_proc.py
+++ python/benchmarks/post_proc.py
@@ -3,14 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-dofs = [31776]  # , 234546, 1801086]
+dofs = [31776, 67281]  # , 234546, 1801086]
 
 
 def visualize_side_by_side(dofs):
     fig, ax = plt.subplots()
+    plt.grid("on", zorder=1)
     procs = []
     first = True
-
+    slaves = []
     for i, dof in enumerate(dofs):
         infile = open("results_bench_{0:d}.txt".format(dof), "r")
 
@@ -18,7 +19,7 @@ def visualize_side_by_side(dofs):
         procs.append(infile.readline().split(": ")[-1].strip("\n"))
         # Skip num dofs
         infile.readline()
-        slaves = int(infile.readline().split(": ")[-1].strip("\n"))
+        slaves .append(int(infile.readline().split(": ")[-1].strip("\n")))
         solve_iterations = int(infile.readline().split(": ")[-1].strip("\n"))
         # Skip info line
         infile.readline()
@@ -31,9 +32,9 @@ def visualize_side_by_side(dofs):
             if data[0] == "Backsubstitution":
                 continue
             if first:
-                plt.bar(i, float(data[2]), 0.5, bottom=total_time, label=data[0], color=colors[j])
+                plt.bar(i, float(data[2]), 0.5, bottom=total_time, label=data[0], color=colors[j], zorder=2)
             else:
-                plt.bar(i, float(data[2]), 0.5, bottom=total_time, color=colors[j])
+                plt.bar(i, float(data[2]), 0.5, bottom=total_time, color=colors[j], zorder=2)
             if data[0] == "Solve":
                 ax.annotate('{0:d}'.format(solve_iterations),
                             xy=(i, total_time + float(data[2]) / 2),
@@ -45,7 +46,8 @@ def visualize_side_by_side(dofs):
         first = False
 
     ax.set_xticks(range(len(dofs)))
-    labels = ["{0:d}\n ({1:s} procs)".format(dof, proc) for dof, proc in zip(dofs, procs)]
+    labels = ["#DOFS {0:d}\n#Slaves {2:d}\n#Procs {1:s}".format(
+        dof, proc, slave) for dof, proc, slave in zip(dofs, procs, slaves)]
     ax.set_xticklabels(labels)
     # ax.set_yscale("log")
     plt.legend()
@@ -53,7 +55,8 @@ def visualize_side_by_side(dofs):
     box = ax.get_position()
     ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
     ax.legend(bbox_to_anchor=(0.95, 0.5))
-    plt.title("Histogram of average runtime of operations")
+    plt.title("Average runtime of operations")
+    plt.ylabel("Runtime (s)")
 
     plt.savefig("test.png")
 
@@ -66,40 +69,34 @@ def visualize_single(dof):
     # Skip num dofs
     infile.readline()
     slaves = int(infile.readline().split(": ")[-1].strip("\n"))
-    solve_iterations = int(infile.readline().split(": ")[-1].strip("\n"))
+    infile.readline()
+    # solve_iterations = int(infile.readline().split(": ")[-1].strip("\n"))
     # Skip info line
     infile.readline()
     # Read timings
     operations = infile.readlines()
     colors = ["tab:blue", "tab:brown", "tab:orange", "tab:green", "tab:red", "tab:purple", "tab:cyan", "tab:olive"]
-    total_time = 0
     fig, ax = plt.subplots()
+    plt.grid("on", zorder=1)
 
     for (j, line) in enumerate(operations):
         data = line.strip("\n").split(" ")
         if data[0] == "Backsubstitution":
             continue
-        plt.bar(j, float(data[2]), 0.5, label=data[0], color=colors[j])
-        if data[0] == "Solve":
-            ax.annotate('{0:d}'.format(solve_iterations),
-                        xy=(j, float(data[2]) / 2),
-                        xytext=(0, 0),
-                        textcoords="offset points",
-                        ha='center', va='bottom')
+        plt.bar(j, float(data[2]), 0.5, label=data[0], color=colors[j], zorder=2)
 
-    ax.set_xticks(range(len(dofs)))
-    labels = ["{0:d}\n ({1:s} procs)".format(dof, proc) for dof, proc in zip(dofs, procs)]
-    ax.set_xticklabels(labels)
-    ax.set_yscale("log")
+    ax.set_xticks([])
+    # ax.set_yscale("log")
     plt.legend()
+    plt.ylabel("Runtime (s)")
     # Shrink current axis by 20%
-    box = ax.get_position()
-    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-    ax.legend(bbox_to_anchor=(0.95, 0.5))
-    plt.title("Average runtime of operations with {0:d} slave DOFs".format(slaves))
+    # box = ax.get_position()
+    # ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+    # ax.legend(bbox_to_anchor=(0.95, 0.5))
+    plt.title("Average runtime of operations with {0:d} slave DOFs of {1:s} processors".format(slaves, procs))
 
     plt.savefig("test_single.png")
 
 
-visualize_single(dofs[0])
+visualize_single(dofs[1])
 visualize_side_by_side(dofs)
