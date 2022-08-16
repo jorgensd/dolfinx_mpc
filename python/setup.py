c33e62e0e9327826bb88e7382db4e371a4a02bf5
Jørgen Dokken <jsd55@cam.ac.uk>
2022-08-16 16:46:28 +0100

    Fix setup py +=

--- python/setup.py
+++ python/setup.py
@@ -46,7 +46,7 @@ class CMakeBuild(build_ext):
         extdir = os.path.abspath(os.path.dirname(
             self.get_ext_fullpath(ext.name)))
         cmake_args = shlex.split(os.environ.get("CMAKE_ARGS", ""))
-        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+        cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
                       '-DPYTHON_EXECUTABLE=' + sys.executable]
 
         cfg = 'Debug' if self.debug else 'Release'
