04ddb46196b1312042a43a877bcbbc8fb5048ced
Florian Monteghetti <f.monteghetti@gmail.com>
2022-08-20 22:04:02 +0200

    Update python/demos/demo_periodic_gep.py
    Co-authored-by: Jørgen Schartum Dokken <dokken92@gmail.com>

--- python/demos/demo_periodic_gep.py
+++ python/demos/demo_periodic_gep.py
@@ -301,6 +301,7 @@ def assemble_and_solve(boundary_condition: List[str] = ["dirichlet","periodic"],
 
 
 
+
     # Diagonal values for slave and Dirichlet DoF
     # The generalized eigenvalue problem will have spurious eigenvalues at
     # lambda_spurious = diagval_A/diagval_B. Here we choose lambda_spurious=1e4,
