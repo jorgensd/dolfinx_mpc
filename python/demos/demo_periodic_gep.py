83e6d500c9681fd37786e6b6d2410a1aaa34632d
Florian Monteghetti <f.monteghetti@gmail.com>
2022-08-20 22:02:49 +0200

    Update python/demos/demo_periodic_gep.py
    Co-authored-by: Jørgen Schartum Dokken <dokken92@gmail.com>

--- python/demos/demo_periodic_gep.py
+++ python/demos/demo_periodic_gep.py
@@ -296,6 +296,7 @@ def assemble_and_solve(boundary_condition: List[str] = ["dirichlet","periodic"],
     b = inner(u, v) * dx
     mass_form = fem.form(a)
     stiffness_form = fem.form(b)
+
     # Diagonal values for slave and Dirichlet DoF
     # The generalized eigenvalue problem will have spurious eigenvalues at
     # lambda_spurious = diagval_A/diagval_B. Here we choose lambda_spurious=1e4,
