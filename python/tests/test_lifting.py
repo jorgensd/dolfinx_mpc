84f67ac5cc75f893b1a258e6762bec0f2070ed3e
Min RK <benjaminrk@gmail.com>
2024-01-24 17:39:10 +0100

    assert_allclose has different default tolerances than allclose

--- python/tests/test_lifting.py
+++ python/tests/test_lifting.py
@@ -114,8 +114,8 @@ def test_lifting(get_assemblers):  # noqa: F811
             reduced_L = K.T @ (L_np)  # - constants)
             # Solve linear system
             d = scipy.sparse.linalg.spsolve(KTAK, reduced_L)
-            # Back substitution to full solution vecto
+            # Back substitution to full solution vector
             uh_numpy = K @ (d)  # + constants)
-            nt.assert_allclose(uh_numpy, u_mpc)
+            nt.assert_allclose(uh_numpy, u_mpc, rtol=1e-5, atol=1e-8)
 
     list_timings(comm, [TimingType.wall])
