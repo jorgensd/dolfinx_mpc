e6465e8b7c4364e19ca7a5b1eaef9a114219a2f6
jorgensd <dokken92@gmail.com>
2025-06-23 17:16:40 +0000

    Fix C++ lifiting check and inner product

--- cpp/lifting.h
+++ cpp/lifting.h
@@ -518,20 +518,24 @@ void apply_lifting(
         "Mismatch in size between a and bcs in assembler.");
   }
   for (std::size_t j = 0; j < a.size(); ++j)
-  {
+  { 
+    if (a[j] and !bcs1[j].empty())
+    {
     if (x0.empty())
-    {
-      impl::apply_lifting<std::complex<double>>(
-          b, a[j], bcs1[j], std::span<const std::complex<double>>(), scale,
-          mpc);
-    }
-    else
-    {
-      impl::apply_lifting<std::complex<double>>(b, a[j], bcs1[j], x0[j], scale,
-                                                mpc);
+      {
+        impl::apply_lifting<std::complex<double>>(
+            b, a[j], bcs1[j], std::span<const std::complex<double>>(), scale,
+            mpc);
+      }
+      else
+      {
+        impl::apply_lifting<std::complex<double>>(b, a[j], bcs1[j], x0[j], scale,
+                                                  mpc);
+      }
     }
   }
 }
+
 
 /// Modify b such that:
 ///
@@ -575,14 +579,17 @@ void apply_lifting(
   }
   for (std::size_t j = 0; j < a.size(); ++j)
   {
-    if (x0.empty())
+    if (a[j] and !bcs1[j].empty())
     {
-      impl::apply_lifting<float>(b, a[j], bcs1[j], std::span<const float>(),
-                                 scale, mpc);
-    }
-    else
-    {
-      impl::apply_lifting<float>(b, a[j], bcs1[j], x0[j], scale, mpc);
+      if (x0.empty())
+      {
+        impl::apply_lifting<float>(b, a[j], bcs1[j], std::span<const float>(),
+                                  scale, mpc);
+      }
+      else
+      {
+        impl::apply_lifting<float>(b, a[j], bcs1[j], x0[j], scale, mpc);
+      }
     }
   }
 }
@@ -633,15 +640,18 @@ void apply_lifting(
   }
   for (std::size_t j = 0; j < a.size(); ++j)
   {
-    if (x0.empty())
+    if (a[j] and !bcs1[j].empty())
     {
-      impl::apply_lifting<std::complex<float>>(
-          b, a[j], bcs1[j], std::span<const std::complex<float>>(), scale, mpc);
-    }
-    else
-    {
-      impl::apply_lifting<std::complex<float>>(b, a[j], bcs1[j], x0[j], scale,
-                                               mpc);
+      if (x0.empty())
+      {
+        impl::apply_lifting<std::complex<float>>(
+            b, a[j], bcs1[j], std::span<const std::complex<float>>(), scale, mpc);
+      }
+      else
+      {
+        impl::apply_lifting<std::complex<float>>(b, a[j], bcs1[j], x0[j], scale,
+                                                mpc);
+      }
     }
   }
 }
--- python/demos/demo_stokes_nest.py
+++ python/demos/demo_stokes_nest.py
@@ -229,7 +229,7 @@ bcs0 = dolfinx.fem.bcs_by_block(dolfinx.fem.extract_function_spaces(L), bcs)
 dolfinx.fem.petsc.set_bc(b, bcs0)
 
 # Preconditioner
-P11 = dolfinx.fem.petsc.assemble_matrix(dolfinx.fem.form(p * q * ufl.dx))
+P11 = dolfinx.fem.petsc.assemble_matrix(dolfinx.fem.form(ufl.inner(p , q) * ufl.dx))
 P = PETSc.Mat().createNest([[A.getNestSubMatrix(0, 0), None], [None, P11]])  # type: ignore
 P.assemble()
 
