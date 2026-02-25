3132cd3f8c2312356257788ffa4291ab0619c579
Jørgen S. Dokken <dokken@simula.no>
2026-02-25 12:28:18 +0100

    Yet another atol increase

--- python/tests/test_multispace_mpc.py
+++ python/tests/test_multispace_mpc.py
@@ -29,7 +29,7 @@ def test_multiple_mpc_spaces_sparsity(cell_type, deg, N):
 
     V = fem.functionspace(domain, element("Lagrange", domain.basix_cell(), deg, dtype=default_real_type))
     Q = V.clone()
-    atol = 500 * np.finfo(default_real_type).eps
+    atol = 5000 * np.finfo(default_real_type).eps
 
     def periodic_boundary(x):
         return np.logical_or(np.isclose(x[0], 1, atol=atol), np.isclose(x[2], 1, atol=atol))
